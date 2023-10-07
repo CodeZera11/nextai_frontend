6381ee7f3163ab11942d6d4751cc29a544d16e79
CodeZera11 <codezera3@gmail.com>
2023-10-07 16:16:37 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-2xl mx-auto p-20 flex gap-5 items-center justify-center">
+      <h1 className="font-bold">Want to integrate with google calendar?</h1>
+      <Link
+        to={"/google-calendar"}
+        className="bg-black text-white px-4 py-2 rounded-lg font-bold hover:text-green-400 transition-colors duration-150 ease-out"
+      >
+        Click here
+      </Link>
     </div>
   );
 }
---
+++ src/components/GoogleCalendar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GoogleCalendar = () => {
+  return (
+    <div>GoogleCalendar</div>
+  )
+}
+
+export default GoogleCalendar

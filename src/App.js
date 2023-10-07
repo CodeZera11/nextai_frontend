f76d9b674ca2dd236cf530dc224c4377a834f98e
CodeZera11 <codezera3@gmail.com>
2023-10-07 20:19:34 +0530

    problem in getting code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const generateAuthUrl = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/google`);
+
+      window.location.href = response.data;
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
   return (
     <div className="max-w-2xl mx-auto p-20 flex gap-5 items-center justify-center">
       <h1 className="font-bold">Want to integrate with google calendar?</h1>
-      <Link
-        to={"/google-calendar"}
-        className="bg-black text-white px-4 py-2 rounded-lg font-bold hover:text-green-400 transition-colors duration-150 ease-out"
+      <button
+        onClick={generateAuthUrl}
+        className="bg-black text-white px-4 py-2 rounded-lg font-bold hover:text-blue-200 transition-colors duration-150 ease-out"
       >
         Click here
-      </Link>
+      </button>
     </div>
   );
 }
--- src/components/GoogleCalendar.jsx
+++ src/components/GoogleCalendar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import axios from "axios";
 
 const GoogleCalendar = () => {
-  return (
-    <div>GoogleCalendar</div>
-  )
-}
+  // const [searchParams] = useSearchParams();
+  // const code = searchParams.get("code");
+  // console.log(code.replace("/", "%"));
 
-export default GoogleCalendar
+  const searchParams = window.location.search;
+  const code = searchParams.split("code=")[1].split("&")[0];
+
+  console.log(code);
+
+  async function fetchEvents(code) {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/get-events",
+        code.replace("/", "%")
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (code) {
+    fetchEvents(code);
+  }
+
+  return <div className="p-20 shadow-2xl">Events data here</div>;
+};
+
+export default GoogleCalendar;

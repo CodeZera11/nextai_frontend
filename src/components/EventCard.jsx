a6d53186cd5783757ad2f265a6899cf19dffe6bc
CodeZera11 <codezera3@gmail.com>
2023-10-08 22:07:14 +0530

    designing event card

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MoveUpRight } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 
 const EventCard = () => {
   return (
     <Link
       to={"/"}
-      className="shadow-xl rounded-xl flex border hover:scale-105 transition-transform duration-300 ease-linear"
+      className="hover:shadow-xl transition-all duration-500 rounded-xl flex border"
     >
       <div className="h-full bg-red-500 w-3 rounded-tl-xl rounded-bl-xl" />
       <div className="flex flex-col gap-2 p-10 flex-1">
@@ -14,8 +14,31 @@ const EventCard = () => {
         <h2 className="font-semibold text-lg">Lunch with Mom</h2>
         <p className="text-gray-400 text-start">Home</p>
       </div>
-      <div className="p-2">
-        <MoveUpRight size={20} />
+      <div className="p-2 flex flex-col justify-between h-full">
+        <div className="hover:text-blue-600 hover:underline underline-offset-4 flex justify-center">
+          Check Event
+          <ArrowUpRight className="" />
+        </div>
+        <div className="flex gap-1">
+          <img
+            src="/profile.jpeg"
+            alt="profile"
+            className="w-[30px] h-[30px] rounded-full"
+            srcset=""
+          />
+          <img
+            src="/profile.jpeg"
+            alt="profile"
+            className="w-[30px] h-[30px] rounded-full"
+            srcset=""
+          />
+          <img
+            src="/profile.jpeg"
+            alt="profile"
+            className="w-[30px] h-[30px] rounded-full"
+            srcset=""
+          />
+        </div>
       </div>
     </Link>
   );

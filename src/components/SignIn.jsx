7b237ff457c4a31464da6dbeca3ec46ff8c44e6c
CodeZera11 <codezera3@gmail.com>
2023-10-10 11:31:19 +0530

    added retry button on signin page

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import Loader from "./Loader";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL;
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const SignIn = () => {
   const [searchParams] = useSearchParams();
+  const [retry, setRetry] = useState(false);
 
   const code = searchParams.get("code");
 
@@ -24,9 +27,11 @@ const SignIn = () => {
       if (response.data.success) {
         window.location.href = `${FRONTEND_URL}/google-calendar`;
       }
+
+      toast.error("Something went wrong!");
+      setRetry(true);
     } catch (error) {
       console.log(error);
-      window.location.href = `${FRONTEND_URL}/google-calendar`;
     }
   };
 
@@ -36,9 +41,22 @@ const SignIn = () => {
   }
 
   return (
-    <div className="flex items justify-center gap-10">
-      Redirecting back to calendar page...
-      <Loader />
+    <div className="">
+      {!retry && (
+        <div className="flex items justify-center gap-10">
+          Redirecting back to calendar page...
+          <Loader />
+        </div>
+      )}
+
+      {retry && (
+        <div className="flex gap-2 items-center justify-center">
+          Something went wrong!
+          <Link to={"/google-calendar"} className="text-blue-500 underline">
+            click here
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
